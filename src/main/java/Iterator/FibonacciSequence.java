58b54fbe3dab030393af56558860feee001a1946
Saed Abukar <saed.a.abukar@gmail.com>
2025-05-04 22:06:17 +0300

    Iterator

--- src/main/java/Iterator/FibonacciSequence.java
+++ src/main/java/Iterator/FibonacciSequence.java
@@ -2,6 +2,20 @@ package Iterator;
 
 import java.util.Iterator;
 
+/**
+ * Initially, I wanted to have the values of 'a' and 'b' in the iterator class and pass them to the sequence class method,
+ * so there would be no need for a for loop, and 'a' and 'b' would be incremented and tracked in the iterator class
+ * alongside the current index position. However, this approach would have violated part 2 of the assignment requirements.
+ *
+ * At the end, I chose to implement a solution where only the current index is saved in the iterator class, and the goal number
+ * in the sequence is saved in the sequence class. The values of 'a' and 'b' are calculated on the fly. This means that 'a' and 'b'
+ * are not saved or tracked, and they are not available anywhere else in the code after their result is computed.
+ *
+ * I would have liked to save 'a' and 'b' in the iterator class and pass them, along with the current index, to the generateFibonacci
+ * method. However, doing so would have meant sharing the responsibility of sequence generation between the classes, which I wanted to avoid.
+ */
+
+
 public class FibonacciSequence implements Sequence {
     private final int n;
     public FibonacciSequence(int n) {
@@ -11,6 +25,7 @@ public class FibonacciSequence implements Sequence {
     public int getN() {
         return n;
     }
+
 
     public int generateFibonacci(int current) {
         if (current <= 2) {
